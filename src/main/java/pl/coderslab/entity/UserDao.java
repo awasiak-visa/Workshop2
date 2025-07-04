b4e5817242daecae606b5d957cc808a3acc1809a
awasiak-visa <awasiak@visa.com>
2025-07-04 13:14:11 +0200

    Method create added

--- src/main/java/pl/coderslab/entity/UserDao.java
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -1,6 +1,9 @@
 package pl.coderslab.entity;
 
 import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.DbUtil;
+
+import java.sql.*;
 
 public class UserDao {
     private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
@@ -11,5 +14,25 @@ public class UserDao {
     public String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
+
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
 }
