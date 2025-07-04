0be164b6274fb701dcaabb4c9314d283e59dcd81
awasiak-visa <awasiak@visa.com>
2025-07-04 15:46:16 +0200

    Tests to method read added

--- src/main/java/pl/coderslab/entity/MainDao.java
+++ src/main/java/pl/coderslab/entity/MainDao.java
@@ -16,5 +16,15 @@ public class MainDao {
         user2.setPassword("passwd");
         userDao.create(user2);
 
+
+        User user3 = userDao.read(7);
+        System.out.println(user3 != null);
+        System.out.println("id: " + user3.getId());
+        System.out.println("userName: " + user3.getUserName());
+        System.out.println("email: " + user3.getEmail());
+        System.out.println("password: " + user3.getPassword());
+
+        User user4 = userDao.read(5);
+        System.out.println(user4 == null);
     }
 }
