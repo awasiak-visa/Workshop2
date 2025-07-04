1635b70fbb6d497d3a26ca761c489dbb04103ef0
awasiak-visa <awasiak@visa.com>
2025-07-04 21:00:09 +0200

    Checking hashed passwords added to tests

--- src/main/java/pl/coderslab/entity/MainDao.java
+++ src/main/java/pl/coderslab/entity/MainDao.java
@@ -1,6 +1,7 @@
 package pl.coderslab.entity;
 
-import java.util.Arrays;
+import org.mindrot.jbcrypt.BCrypt;
+
 
 public class MainDao {
     public static void main(String[] args) {
@@ -12,30 +13,34 @@ public class MainDao {
         user1.setEmail("karol@gmail.com");
         user1.setPassword("123456");
         userDao.create(user1);
+        System.out.println(user1.getPassword());
 
         User user2 = new User();
         user2.setUserName("hania");
         user2.setEmail("hanna@outlook.com");
         user2.setPassword("passwd");
         userDao.create(user2);
+        System.out.println(BCrypt.checkpw("passwd", user2.getPassword()));
 
         // Tests for method read.
-        User user3 = userDao.read(7);
+        User user3 = userDao.read(3);
         System.out.println(user3 != null);
         System.out.println("id: " + user3.getId());
         System.out.println("userName: " + user3.getUserName());
         System.out.println("email: " + user3.getEmail());
         System.out.println("password: " + user3.getPassword());
 
-        User user4 = userDao.read(5);
+        User user4 = userDao.read(100);
         System.out.println(user4 == null);
 
         // Test for method update.
-        user3.setUserName("ala");
-        user3.setEmail("ala@gmail.com");
+        user3.setUserName("alicja");
+        user3.setEmail("alicja@gmail.com");
         user3.setPassword("1234");
         userDao.update(user3);
         System.out.println("id: " + user3.getId());
+        System.out.println(user3.getPassword());
+        System.out.println(BCrypt.checkpw("1234", user3.getPassword()));
 
         // Test for method delete.
         userDao.delete(1);
